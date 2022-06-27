1221b5eeffb35fefd15beef762146e68c02a3ec7
MorisReal <108234204+MorisReal@users.noreply.github.com>
2022-06-27 16:50:13 +0300

    3d text

---
+++ app.js
@@ -0,0 +1,35 @@
+let text3d = new Ztextify(
+    "h2",
+    {
+        depth:"30px",
+        layers:10,
+        fade: true,
+        direction:"both",
+        event: "pointer",
+        eventRotation:"30deg"
+    }
+);
+
+ let img3d = new Ztextify(".image",
+ {
+     depth:"30px",
+     layers:10,
+     fade:true,
+     direction:"borth",
+     event:"pointer",
+     eventDirection:"reverse",
+     eventRotation:"35deg",
+     perspective: "500px"
+ }
+ );
+ let img3d1 = new Ztextify(".foto",
+    {
+        depth:"30px",
+        layers:"10",
+        fade:false,
+        direction:"both",
+        event:"pointer",
+        eventRotation:"45deg"
+    }
+
+ );
